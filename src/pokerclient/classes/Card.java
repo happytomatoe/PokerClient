5bb44ef2da44a9b1cb85d61a146c9db7282ff9e3
Roman <babkamen@gmail.com>
2013-03-17 23:12:05 +0200

    dsad
    dasda

---
+++ src/pokerclient/classes/Card.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokerclient.classes;
+
+/**
+ *
+ * @author babkamen
+ */
+public class Card {
+    
+}
